aa3fea971124ba5598f069c719d4832cd93680fd
great084 <great084@gmail.com>
2020-04-06 15:01:56 +0900

    import libs and create graphs controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
+// ********* 以下を追加 ******
+//= require Chart.min
+//= require flatpickr
+//= require flatpickr/l10n/ja
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+// ********* 以上を追加 ******
 //= require turbolinks
 //= require_tree .
